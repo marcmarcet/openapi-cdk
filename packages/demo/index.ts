51264742672079273cd8d3b977b1995345fe329f
Marc Marcet <marc.marcet@codinghumans.com>
2021-08-30 20:20:43 +0200

    chore: remove unused variable

--- packages/demo/index.ts
+++ packages/demo/index.ts
@@ -2,7 +2,7 @@ import { App, OpenApiV3 } from 'cdkoa';
 
 var app = new App({});
 
-var x = new OpenApiV3.Document(app, 'my-api', {
+new OpenApiV3.Document(app, 'my-api', {
   openapi: '3.0.0',
   info: null!,
   paths: null!
