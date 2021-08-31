243f25ae4d3f1ed20be38a6624d0695d61bfbfe6
Marc Marcet <marc.marcet@codinghumans.com>
2021-08-31 18:22:00 +0200

    chore: flatten files

--- packages/demo/bin/app.ts
+++ packages/demo/bin/app.ts
@@ -1,10 +1,6 @@
 import { App, OpenApiV3 } from 'cdkoa';
-import { Info, Paths } from '../../cdkoa/lib/openapi-v3';
+import { Info, Operation, PathItem, Paths } from '../../cdkoa/lib/openapi-v3';
 import description from '../lib/info/description';
-import pet from '../lib/paths/pet';
-import petFindByStatus from '../lib/paths/pet-findByStatus';
-import petFindByTags from '../lib/paths/pet-findByTags';
-import petPetId from '../lib/paths/pet-petId';
 
 var app = new App({});
 
@@ -16,10 +12,48 @@ new OpenApiV3.Document(app, 'my-api', {
     termsOfService: 'http://swagger.io/terms/'
   }),
   paths: new Paths({
-    '/pet': pet,
-    '/pet/{petId}': petPetId,
-    '/pet/findByStatus': petFindByStatus,
-    '/pet/findByTags': petFindByTags,
+    '/pet': new PathItem({
+      summary: 'Pet endpoint',
+      description: 'Pet endpoint',
+      post: new Operation({
+        tags: ['pet'],
+        summary: 'Add a new pet to the store',
+        description: 'Add a new pet to the store',
+        operationId: 'addPet',
+        deprecated: false,
+      }),
+      put: new Operation({
+        tags: ['pet'],
+        summary: 'Update an existing pet',
+        description: 'Update an existing pet by Id',
+        operationId: 'updatePet',
+        deprecated: false,
+      })
+    }),
+    '/pet/{petId}': new PathItem({
+      get: new Operation({
+        tags: ['pet'],
+        operationId: 'getPetById',
+        summary: 'Find pet by ID',
+        description: 'Returns a single pet',
+      })
+    }),
+    '/pet/findByStatus': new PathItem({
+      get: new Operation({
+        tags: ['pet'],
+        operationId: 'findPetsByStatus',
+        summary: 'Finds Pets by status',
+        description: 'Multiple status values can be provided with comma separated strings'
+      })
+    }),
+    '/pet/findByTags': new PathItem({
+      get: new Operation({
+        tags: ['pet'],
+        operationId: 'findPetsByTags',
+        summary: 'Finds Pets by tags',
+        description: `Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.`,
+      })
+    }),
   })
 });
 
